0ad386a3eb130c976afd46e013fffa1a83eae3cb
Marigela Bhavana <marigelabhavana2000@gmail.com>
2024-02-15 17:59:00 +0530

    success

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Fibonacci Series");
+int n = 10;
+int a = 0, b = 1;
+for (int i = 0; i < n; i++)
+{
+    Console.WriteLine("{0}", a);
+    int c = a + b;
+    a = b;
+    b = c;
+}
